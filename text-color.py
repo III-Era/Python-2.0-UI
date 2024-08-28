bc8534ff4fb5c3b50b1154cc5a8dc80adcb7b1d2
III-Era <ferko@vp.pl>
2024-08-28 19:38:47 +0200

    Update text-color.py

--- text-color.py
+++ text-color.py
@@ -1,1 +1,9 @@
-
+redcolor = "red"
+yellowcolor = "yellow"
+bluecolor = "blue"
+whitecolor = "white"
+def colorinput(text="", color=whitecolor):
+  if color == "white":
+    input(text)
+  elif color == "red":
+    input(f"\033[31m{text}")
